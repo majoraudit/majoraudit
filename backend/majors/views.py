32c93984e850c08aed68c6df6299f62823bc66c2
Darren Kao <nobelium100@gmail.com>
2025-11-20 00:26:34 -0500

    Added one url and view for getting major info

--- backend/majors/views.py
+++ backend/majors/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
+from django.db.models import Prefetch
+from .models import Course, CourseInstance
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+
+class MajorIDView(generics.ListAPIView):
+
+    def get_queryset(self, major_id):
+        json_data = open('./major_templates/' + major_id + "/" + major_id + ".json")   
+        data = json.load(json_data) 
+        return HttpResponse(json.dumps(data), content_type='application/json')
