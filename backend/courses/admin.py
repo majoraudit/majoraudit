4bc6d14aa3d40e714b07108838b71d453a2041cd
winbow13 <benjaminwu13@BWThinkPad.localdomain>
2026-02-22 14:08:09 -0500

    Added course tags to models and script

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Course, CourseInstance, CourseCode, CourseProfessor
+from .models import Course, CourseTag, CourseInstance, CourseCode, CourseProfessor
 
 # Register your models here.
 admin.site.register(Course)
+admin.site.register(CourseTag)
 admin.site.register(CourseInstance)
 admin.site.register(CourseCode)
 admin.site.register(CourseProfessor)
